1dd3ba9f067459a16c02743b52c52b5f5d1bcb2f
Phillip <yuaimelin1314@126.com>
2017-12-30 08:41:10 +0800

    主界面搭建外层封装

---
+++ common/src/main/java/com/im/yutalker/common/widget/PortraitView.java
@@ -0,0 +1,25 @@
+package com.im.yutalker.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Phillip on 2017/12/29.
+ */
+
+public class PortraitView extends CircleImageView {
+
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
